0188ca2cc4d340f948393e6fe5e14bdd48832b8a
lelaboratoire <grixor@gmail.com>
2018-10-25 17:03:53 -0400

    Add jupyter notebook to run TPOT-DS on simulated data

--- simulation/tpot_light_sim_genex_pipeline.py
+++ simulation/tpot_light_sim_genex_pipeline.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
+from sklearn.cluster import FeatureAgglomeration
 from sklearn.feature_selection import SelectPercentile, f_classif
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import Normalizer
+from sklearn.tree import DecisionTreeClassifier
 from tpot.builtins import DatasetSelector
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
@@ -13,12 +13,12 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=None)
 
-# Average CV score on the training set was:0.7465628476084539
+# Average CV score on the training set was:0.7118946978123841
 exported_pipeline = make_pipeline(
     DatasetSelector(sel_subset=0, subset_list="subsets.csv"),
-    SelectPercentile(score_func=f_classif, percentile=52),
-    Normalizer(norm="max"),
-    KNeighborsClassifier(n_neighbors=11, p=1, weights="uniform")
+    SelectPercentile(score_func=f_classif, percentile=75),
+    FeatureAgglomeration(affinity="l2", linkage="complete"),
+    DecisionTreeClassifier(criterion="entropy", max_depth=9, min_samples_leaf=1, min_samples_split=4)
 )
 
 exported_pipeline.fit(training_features, training_target)
