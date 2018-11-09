99e80f351cd51c940f1686a8bec9487a20cf321c
Trang Le <grixor@gmail.com>
2018-11-08 20:43:06 -0500

    edit plot to boxplot and beeswarm

--- RNASeq/visualize_ds.R
+++ RNASeq/visualize_ds.R
@@ -4,6 +4,10 @@ library(reshape2)
 library(tidyverse)
 library(ggthemes)
 library(scales)
+library(ggplot2)
+library(ggforce)
+# library(viridis)
+# library(colorblindr)
 
 n.iters <- 44
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
@@ -50,10 +54,13 @@ p <- ggplot(accu.subset, aes(group = subidx)) +
 
 p
 
-q <- ggplot(accu.subset, aes(group = subidx)) + 
-  geom_violin(aes(x = subidx, y = `Testing Accuracy`, fill = selectedSubsetID), 
-              alpha = 0.6, scale = "count", trim = T) +
-  geom_jitter(aes(x = subidx, y = (`Testing Accuracy`), color = selectedSubsetID),  size = 1) + 
+accu.subset$subname <- as.factor(paste0("s", accu.subset$subidx))
+accu.subset$subname <- factor(accu.subset$subname, levels = unique(accu.subset$subname))
+q <- ggplot(accu.subset, aes(x = subname, y = `Testing Accuracy`)) + 
+  geom_boxplot(color = "grey80") +
+  # geom_violin(aes(x = subidx, y = `Testing Accuracy`, fill = selectedSubsetID), 
+  #             alpha = 0.6, scale = "count", trim = T) +
+  geom_jitter(size = 2, width = 0.1) + 
   theme_bw() + 
   # viridis::scale_fill_viridis(discrete = TRUE, option="D", reverse = TRUE) +
   # viridis::scale_color_viridis(discrete = TRUE, option="D", reverse = TRUE) +
@@ -61,12 +68,41 @@ q <- ggplot(accu.subset, aes(group = subidx)) +
   # scale_fill_tableau("Classic Color Blind") +
   # scale_color_colorblind() +
   # scale_fill_colorblind() +
-  scale_fill_viridis_d(direction = -1, end = 0.5) + 
-  scale_color_viridis_d(direction = -1, end = 0.5) +
-  scale_x_continuous(breaks = seq(1, 23, 3), limits = c(1, 23), minor_breaks = 1:23) + 
+  # scale_fill_viridis_d(direction = -1, end = 0.5) +
+  # scale_color_viridis_d(direction = -1, end = 0.5) +
+  # scale_x_continuous(breaks = seq(1, 23, 3), limits = c(1, 23), minor_breaks = 1:23) + 
   labs(x = "Subset ID", y = "Testing Accuracy") +
   guides(fill = FALSE) + guides(colour=FALSE)
 
 q
 
 ggsave(q, filename = paste0("real_", n.iters, ".svg"), width = 5, height = 4, units = "in")
+
+
+
+
+
+
+q <- ggplot(accu.subset, aes(x = subname, y = `Testing Accuracy`)) + 
+  geom_boxplot(color = "grey70") +
+  # geom_violin(aes(x = subidx, y = `Testing Accuracy`, fill = selectedSubsetID), 
+  #             alpha = 0.6, scale = "count", trim = T) +
+  # geom_jitter(size = 2, width = 0.1, height = 0) +
+  # ggforce::geom_sina(size = 2) +
+  ggbeeswarm::geom_beeswarm(cex = 2, size = 2, alpha = 0.7) +
+  theme_bw() + 
+  # viridis::scale_fill_viridis(discrete = TRUE, option="D", reverse = TRUE) +
+  # viridis::scale_color_viridis(discrete = TRUE, option="D", reverse = TRUE) +
+  # scale_colour_tableau("Classic Color Blind") +
+  # scale_fill_tableau("Classic Color Blind") +
+  # scale_color_colorblind() +
+  # scale_fill_colorblind() +
+  # scale_fill_viridis_d(direction = -1, end = 0.5) +
+  # scale_color_viridis_d(direction = -1, end = 0.5) +
+  # scale_x_continuous(breaks = seq(1, 23, 3), limits = c(1, 23), minor_breaks = 1:23) + 
+  labs(x = "Subset ID", y = "Testing Accuracy") +
+  guides(fill = FALSE) + guides(colour=FALSE)
+
+q
+
+
