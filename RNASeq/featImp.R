d675cd722ab206822fa199e65941cc482d9ecbc2
lelaboratoire <grixor@gmail.com>
2018-11-26 16:38:55 -0500

    add feature importance scores of best pipelines 81 (DGM-5) and 59 (DGM-13)

---
+++ RNASeq/featImp.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(cowplot)
+
+feat.imp <- read.csv("featureImp5.csv")[,-1]
+feat.imp <- feat.imp[order(feat.imp$score, decreasing = T),]
+write.csv(feat.imp, file = "featureImportance5.csv")
+
+feat.imp$feat <- factor(feat.imp$feat, levels = feat.imp$feat )
+p <- ggplot(feat.imp[1:20,], aes(x=score, y=fct_reorder(feat, score, fun=identity))) + 
+  geom_point(color = "grey40", stroke = 0) + theme_bw() +
+  labs(x = "DGM-5", y = "") + 
+  scale_x_continuous(limits = c(0.007, 0.04))
+# ggsave(p, filename = "importanceFeatures5.svg", height = 4, width = 7)
+
+feat.imp <- read.csv("featureImp12.csv")[,-1]
+feat.imp <- feat.imp[order(feat.imp$score, decreasing = T),]
+write.csv(feat.imp, file = "featureImportance12.csv")
+
+feat.imp$feat <- factor(feat.imp$feat, levels = feat.imp$feat )
+q <- ggplot(feat.imp[1:20,], aes(x=score, y=fct_reorder(feat, score, fun=identity))) + 
+  geom_point(color = "grey40", stroke = 0) + theme_bw() +
+  scale_x_continuous(limits = c(0.007, 0.04)) +
+  labs(x = "DGM-13", y = "")  
+# ggsave(q, filename = "importanceFeatures12.svg", height = 4, width = 7)
+
+pq <- plot_grid(p, q, labels="AUTO")
+ggsave(pq, filename = "importanceFeatures.svg", height = 4, width = 7)
